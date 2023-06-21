11b04e8bb7fc47daeef6533536f68e5a18cd8d89
NineTwoProgramming <marasimowicz@ymail.com>
2023-06-21 18:54:54 -0400

    added mutations to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,9 +14,20 @@ const typeDefs = gql `
         country: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
         users: [User]
         user(email: String!): User
+    }
+
+    type Mutation {
+        login (email: String!, password: String!): Auth
+        addUser (email: String!, password: String!): Auth
+        addSearch (email: String!, city: String!, state: String!, country: String!): Search
     }
 `;
 
