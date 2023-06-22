4def8a873cf5af89e2260a3c385cebcd807c752e
NineTwoProgramming <marasimowicz@ymail.com>
2023-06-22 17:41:13 -0400

    testing out routes

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -13,6 +13,6 @@ router.route("/").post(createUser).put(authMiddleware);
 
 router.route("/login").post(login);
 
-router.route("/myWeather").get(authMiddleware, getSingleUser);
+router.route("/me").get(authMiddleware, getSingleUser);
 
 module.exports = router;
